605bb1edbce68d1fcb84dd0f7cddd0a721259dc8
finedsoft <finedsoft@gmail.com>
2023-12-28 20:37:13 +0530

    shift and unshift

---
+++ shift_and_unshift.js
@@ -0,0 +1,12 @@
+let history = [ 'page 2', 'page 1'];
+
+let currentPage = history[0]
+console.log(currentPage)
+
+history.unshift('page 4','page 3');
+
+console.log(history);
+
+history.shift()
+
+console.log(history);
