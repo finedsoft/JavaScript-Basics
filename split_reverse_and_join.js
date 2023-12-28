3503de715c3355264dfa470deb1bb1d34183aeef
finedsoft <finedsoft@gmail.com>
2023-12-28 23:09:02 +0530

    split reverse and join

---
+++ split_reverse_and_join.js
@@ -0,0 +1,15 @@
+let datetime = "2022-12-31T12:48:12.123Z";
+
+let date = datetime.split("T")
+
+console.log(date[0].split('-').reverse().join("/"))
+
+let str = "radars"
+
+console.log(str.split('').reverse().join('') === str)
+
+let title= "Check if a number is Palindrome"
+
+let gfgUrl = "https://www.geeksforgeeks.org/check-if-a-number-is-palindrome"
+
+console.log(title.split(' ').join('-').toLowerCase())
